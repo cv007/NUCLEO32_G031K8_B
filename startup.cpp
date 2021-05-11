12bce62a8b8871d726d215a9238821f12968944e
cv007 <32439630+cv007@users.noreply.github.com>
2021-05-11 02:18:30 -0500

    Add files via upload

--- startup.cpp
+++ startup.cpp
@@ -14,28 +14,13 @@ using flashVectorT = struct { u32* stackTop; void(*vfunc[3])(); };
 
 /*-----------------------------------------------------------------------------
     vars and constants
-
-    .ramvector section added to linker script, the section starts at the
+    (.ramvector section added to linker script, the section starts at the
     first location in ram so is aligned properly, _sramvector/_eramvector
-    values added so we can init ram vector table in this startup code
-
-    symbols declared as an array as we need in array form to outsmart
-    the compiler (-Warray_bounds) which will not let us cast _sramvector in 
-    a way so we can set the stack/reset values seperately, so just set to a
-    size of 3 so we can access the first two, and use the address of the 
-    third (_eramvector rakes care of finding the end when we init the vector
-    values)
-
-    the array form also works for other symbols where only the address is 
-    used, and the array of 1 is a pointer but then does not later require
-    the use of &, or if declaring the extern as a pointer we don't end up
-    using as a pointer to a pointer
+    values added so we can init ram vector table in this startup code)
 -----------------------------------------------------------------------------*/
 //linker script symbols
 extern u32 _etext       [1];    //end of text
-extern u32 _sramdebug   [32];   //ram set aside for debug use
-extern u32 _eramdebug   [1];
-extern u32 _sramvector  [3];    //start of ram vector
+extern u32 _sramvector  [1];    //start of ram vector
 extern u32 _eramvector  [1];
 extern u32 _srelocate   [1];    //data (initialized)
 extern u32 _erelocate   [1];
@@ -45,8 +30,6 @@ extern u32 _estack      [1];
 
 //setup linker symbols as u32 pointers (addresses), and nicer names
 static constexpr auto dataFlashStart    { _etext };
-static constexpr auto debugramStart     { _sramdebug };
-static constexpr auto debugramEnd       { _eramdebug };
 static constexpr auto ramvectorStart    { _sramvector };
 static constexpr auto ramvectorEnd      { _eramvector };
 static constexpr auto dataStart         { _srelocate };
@@ -55,9 +38,8 @@ static constexpr auto bssStart          { _szero };
 static constexpr auto bssEnd            { _ezero };
 static constexpr auto stackTop          { _estack };
 
-//SCB.VTOR (vector table offset), SCB.AIRCR (for swReset)
+//SCB.VTOR (vector table offset)
 static volatile u32&  VTOR              { *(u32*)0xE000ED08 };
-static volatile u32&  AIRCR             { *(u32*)0xE000ED0C };
 
 //for delay functions
 static constexpr u32  FCPU_MHZ          {16}; //16MHz at reset
@@ -68,8 +50,9 @@ static constexpr u32  CYCLES_PER_LOOP   {4};
 -----------------------------------------------------------------------------*/
 int main();
 static void resetFunc();
-extern "C" void __libc_init_array();
 static void errorFunc();
+extern "C" void __libc_init_array();
+
 
 /*-----------------------------------------------------------------------------
     reset vector block, 4 words -
@@ -80,7 +63,7 @@ static void errorFunc();
     section is KEEP in linker script, 'used' keeps compiler from complaining
 -----------------------------------------------------------------------------*/
 [[ using gnu : section(".vectors"), used ]]
-flashVectorT flashVector{ stackTop, {resetFunc, errorFunc, errorFunc } };
+flashVectorT flashVector{ stackTop, {resetFunc, errorFunc, errorFunc} };
 
 
 /*-----------------------------------------------------------------------------
@@ -89,73 +72,35 @@ flashVectorT flashVector{ stackTop, {resetFunc, errorFunc, errorFunc } };
                 #pragma GCC push_options
                 #pragma GCC optimize ("-Os")
                 IIA
-delayCycles     (volatile u32 n) { while( n -= CYCLES_PER_LOOP, n>CYCLES_PER_LOOP ){} }
-                IIA 
+delayCycles     (volatile u32 n) { while(n -= CYCLES_PER_LOOP, n > CYCLES_PER_LOOP){} }
+                IIA
 delayMS         (u16 ms){ delayCycles(FCPU_MHZ*1000*ms); }
                 #pragma GCC pop_options
-
 
                 IIA //start address, end address, value
 setmem          (u32* s, u32* e, u32 v) { while(s < e) *s++ = v; }
                 IIA //start address, end address, values
 cpymem          (u32* s, u32* e, u32* v) { while(s < e) *s++ = *v++; }
 
-
-                //software reset
-                [[ using gnu : used, noreturn ]] static void
-swReset         () 
-                {
-                asm( "dsb 0xF":::"memory");
-                AIRCR = 0x05FA0004;
-                asm( "dsb 0xF":::"memory");
-                while( true ){}
-                }
-
-                //unhandled interrupt, exception, or return from main
-                [[ using gnu : used, noreturn ]] static void
-errorFunc       () 
-                { 
-                //get main stack pointer, get the 8 stack entries which
-                //were possibly set by exception (r0-r3,r12,lr,pc,xpsr)
-                //and copy to debug ram
-                u32* msp;
-                asm( "MRS %0, msp" : "=r" (msp) );
-                for( auto i = 0; i < 8; i++ ) {
-                    debugramStart[i] = msp[i];
-                    }
-                //then software reset
-                swReset();
-                }
+                //unhandled interrupt, or return from main
+                //do something here to debug, or to recover when no longer
+                //debugging (system reset, or something)
+                [[ using gnu : used, noreturn ]]
+                static void
+errorFunc       () { while(true); }
 
                 IIA
-initRamDebug    ()
-                {
-                //if last debug ram not set to key value, 
-                //clear debug ram and set to key value
-                if( debugramStart[31] != 0x12345678 ) {
-                    setmem( debugramStart, debugramEnd, 0 );
-                    debugramStart[31] = 0x12345678;
-                    }
-                //else inc reset count, and leave debug ram as-is
-                //(may be exception data stored we want to see later)
-                else debugramStart[30]++;
-                }
-
-                IIA
-initRamVectors  ()
+initVectors     ()
                 {
                 //set all ram vectors to default function (errorFunc)
-                setmem( &ramvectorStart[2], ramvectorEnd, (u32)errorFunc );
-                //set stack/reset in case code uses vtor to read these values
-                //(they are in flash, so should rad from there if needed, but...)
-                ramvectorStart[0] = (u32)stackTop;
-                ramvectorStart[1] = (u32)resetFunc;
+                //(stack and reset also set, should be harmless)
+                setmem( ramvectorStart, ramvectorEnd, (u32)errorFunc );
                 //move vectors to ram
                 VTOR = (u32)ramvectorStart;
                 }
 
                 IIA
-initRamData     ()
+initRam         ()
                 {
                 //init data from flash
                 cpymem( dataStart, dataEnd, dataFlashStart );
@@ -163,21 +108,16 @@ initRamData     ()
                 setmem( bssStart, bssEnd, 0 );
                 }
 
-                IIA
-initRam         ()
-                {
-                initRamDebug();
-                initRamVectors();
-                initRamData();
-                }
-
                 [[ using gnu : used, noreturn ]]
                 static void
 resetFunc       ()
                 {
-                delayMS(5000);          //allow time to allow swd hot-plug
-                initRam();              //ram vectors, normal data/bss init, etc.               
-                __libc_init_array();    //libc init, c++ constructors, etc.
+                delayMS(5000);  //time to allow swd hot-plug
+                initVectors();  //setup ram vectors
+                initRam();      //normal data/bss init
+
+                //libc init, c++ constructors, etc.
+                __libc_init_array();
 
                 //C++ will not allow using main with pendatic on, so disable pedantic
                 #pragma GCC diagnostic push
