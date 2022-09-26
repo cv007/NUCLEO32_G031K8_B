0661aa92c2765b89ed8a1fb540d65d55e6ac50f6
cv007 <32439630+cv007@users.noreply.github.com>
2022-09-25 19:17:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,6 @@
     stm32g031k8 - nucleo32
 --------------------------------------------------------------*/
 #if 0
-                int
-main            ()
-                {
-                u32 count = 0;
-                    uart
-                        << FG GREEN "Hello World: "
-                        << Hex0x << setwf(8,'0') << count++
-                        << endl;
-                    uart2
-                        << FG GREEN "Hello World: "
-                        << Hex0x << setwf(8,'0') << count++
-                        << endl;
-                    }
-                    delayMS(10);
-                }
-#endif
-
-#if 0
 #include "Encoder.hpp"
 /*-------------------------------------------------------------
     main
@@ -46,32 +28,6 @@ encoder1Init    ()
                 int
 main            ()
                 {
-                // uart << endl;
-
-                // uart << Hex0x << setwf(15,' ') << "RCC APBENR1: " << setwf(8,'0') << RCC->APBENR1;
-                // RCC->APBENR1 or_eq (1<<28); //PWREN
-                // uart << " -> " << setwf(8,'0') << RCC->APBENR1 << endl;
-
-                // uart << Hex0x << setwf(15,' ') << "PWR CR1: " << setwf(8,'0') << PWR->CR1;
-                // PWR->CR1 or_eq (1<<8);  //DBP disable rtc domain write protection
-                // uart << " -> " << setwf(8,'0') << PWR->CR1 << endl;
-
-                // uart << Hex0x << setwf(15,' ') << "RCC BDCR: " << setwf(8,'0') << RCC->BDCR;
-                // RCC->BDCR = 1; //LSEON
-                // uart << " -> " << setwf(8,'0') << RCC->BDCR; //LSEON should be set
-                // while( (RCC->BDCR bitand 2) == 0 ){} //LSERDY
-                // uart << " -> " << setwf(8,'0') << RCC->BDCR << endl2r; //LSERDY is set
-
-/*
-
-  RCC APBENR1: 0x00020000 -> 0x10020000
-      PWR CR1: 0x00000208 -> 0x00000308
-     RCC BDCR: 0x00000003 -> 0x00000003 -> 0x00000003
-
-*/
-// while(1){}
-
-
                 encoder1Init();
 
                 i32 lastc = 0;
@@ -131,135 +87,6 @@ main            ()
 
 #endif
 
-
-//troubleshoot why no irq
-#if 0
-/*-------------------------------------------------------------
-    main
-        instances available from headers-
-        board
-        uart
---------------------------------------------------------------*/
-//D4 (PA10) on Nucleo32
-auto sw = GpioPin(board.D[4],LOWISON)
-                .mode(INPUT)
-                .pull(PULLUP)
-                .irqMode(FALLING);
-
-                int
-main            ()
-                {
-                //set irq function for sw irq
-                irqFunction(
-                    EXTI4_15_IRQn,
-                    [](){
-                        sw.isFallFlag(); //reading also clears flag
-                        //stay here, show we are in isr
-                        while(1){
-                            board.led.toggle();
-                            delayMS(100);
-                            }
-                    }
-                );
-
-                auto count = 0;
-                while( true ){
-                    delayMS( 5000 );
-                    uart
-                        << setw(10) << "count: " << count++ << endl
-                        << hex << showbase << uppercase << setfil('0')
-                        << setw(10) << "RTSR1: " << EXTI->RTSR1 << endl
-                        << setw(10) << "FTSR1: " << EXTI->FTSR1 << endl
-                        << setw(10) << "RPR1: " << EXTI->RPR1 << endl
-                        << setw(10) << "FPR1: " << EXTI->FPR1 << endl
-                        << setw(10) << "EXTICR: " << EXTI->EXTICR[0] << endl
-                        << setw(10) << ": " << EXTI->EXTICR[1] << endl
-                        << setw(10) << ": " << EXTI->EXTICR[2] << endl
-                        << setw(10) << ": " << EXTI->EXTICR[3] << endl
-                        << setw(10) << "IMR1: " << EXTI->IMR1 << endl
-                        << setw(10) << "EMR1: " << EXTI->EMR1 << endl
-                        << setw(10) << "ISER: " << NVIC->ISER[0U] << endl
-                        << setw(10) << "ISPR: " << NVIC->ISPR[0U] << endl
-                        << setw(10) << "scbICSR: " << SCB->ICSR << endl
-                        << setw(10) << "ITLINE7: " << SYSCFG->IT_LINE_SR[7] << endl
-                        << setw(10) << "VTOR: " << SCB->VTOR << endl
-                        << setw(10) << "[23]: " << ((u32*)(SCB->VTOR))[23] << endl
-
-                    //startup.cpp will store scbICSR value first thing in reset function
-                    //and the value should be 0, but is 3 after programming (exception irq is active)
-                    //(startup.cpp also sets exception handler to reset function address so we get
-                    //here to print out the data- otherwise stuck in esception handler)
-
-                    //since we are in an exception in these cases, no irq even though all other
-                    //registers indicate everything is setup correctly
-
-                        << "[01]: " << ((u32*)(SCB->VTOR))[01] << endl << clear;
-                    }
-
-                }
-
-#endif
-
-
-
-#if 0
-/*-------------------------------------------------------------
-    main
---------------------------------------------------------------*/
-//arrays of pins
-GpioPin switches[]{ //nucleo board labels D9-D12
-    { GpioPin(board.D[12],LOWISON).mode(INPUT).pull(PULLUP) },
-    { GpioPin(board.D[11],LOWISON).mode(INPUT).pull(PULLUP) },
-    { GpioPin(board.D[10],LOWISON).mode(INPUT).pull(PULLUP) },
-    { GpioPin(board.D[9],LOWISON).mode(INPUT).pull(PULLUP) }
-};
-
-                bool
-switchValues    (u32& swv) //return true if switches changed
-                {
-                static u32 last;
-                swv = 0;
-                for( auto& s : switches ) {
-                    swv <<= 1;
-                    if( s.isOn() ) swv++;
-                    }
-                if( last != swv ) {
-                    last = swv;
-                    return true;
-                    }
-                return false;
-                }
-
-                void
-printInfo       (u32 swv, u32 t)
-                {
-                uart
-                    << "switches: " << setfill('0') << bin
-                    << setw(4) << swv
-                    << " delay: " << dec << t << endl;
-                }
-
-                int
-main            ()
-                {
-                u32 swState = 0;
-                const u32 tbase = 100;
-
-                while( true ) {
-                    //get all switch states
-                    bool changed = switchValues(swState);
-                    //calculate delay value
-                    auto t = tbase + swState*200;
-                    //print info if switch state changes
-                    if( changed ) printInfo( swState, t );
-                    //delay and toggle led
-                    delayMS( t );
-                    board.led.toggle();
-                    }
-
-                }
-
-#endif
 
 #if 1
 /*-------------------------------------------------------------
